8f4fa7e14c19bc2403783bf41fc2a805af9cf751
godzzz <g0dzZz1lla@yandex.ru>
2024-04-13 21:38:37 +0400

    [v0.0.1] First release

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Template.Package.Unity
+# Game.Loop
---
+++ Runtime/IRestartService.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System;
+
+namespace Depra.Game.Loop
+{
+	public interface IRestartService
+	{
+		event Action Restarted;
+
+		void Restart();
+	}
+}
